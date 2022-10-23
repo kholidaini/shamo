a90919f71cacc799a166bec3e398f7f329617c5b
kholidaini <kholidainii@gmail.com>
2022-10-23 17:50:13 +0700

    edit register

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use App\Helpers\ResponseFormatter;
 use Laravel\Fortify\Rules\Password;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
@@ -30,15 +31,16 @@ class UserController extends Controller
                 'password' => Hash::make($request->password),
             ]);
             $user = User::where('email', $request->email)->first();
-            $tokenResult = $user->createToken('authTOken')->plainTextToken;
+            $tokenResult = $user->createToken('authToken')->plainTextToken;
 
             return ResponseFormatter::success([
                 'access_token' => $tokenResult,
                 'token_type' => 'Bearer',
                 'user' => $user,
             ], 'User Registered');
-        } catch (Exception $error) {
-            return ResponseFormatter::success(
+        }
+        catch (Exception $error) {
+            return ResponseFormatter::error(
                 [
                     'massage' => 'something went wrong',
                     'error' => $error
@@ -46,6 +48,42 @@ class UserController extends Controller
                 'authentication failed',
                 500
             );
+        }
+    }
+
+    public function login(Request $request)
+    {
+        try{
+            $request-> validate([
+                'email'=> 'email|required',
+                'password' => 'required',
+            ]);
+
+
+            $credentials = request(['email','password']);
+            if(!Auth::attempt($credentials)){
+                return ResponseFormatter::error([
+                    'message' => 'Unauthorized'],
+                    'Authentication failed',500);
+            }
+
+            $user = User::where('email',$request->email)->first();
+            if(! Hash::check($request->password, $user->password [])){
+                throw new \Exception('invalid credentials');
+            }
+
+            $tokenResult = $user->createToken('authtoken')->plainTextToken;
+            return ResponseFormatter::success([
+                'access_token'=>$tokenResult,
+                'token_type'=> 'Bearer',
+                'user'=> $user
+            ],'Authenticated');
+
+        } catch(Exception $error){
+            return ResponseFormatter::error([
+            'message' => 'Something wrong',
+            'error' => $error],
+            'Authentication Failed',500);
         }
     }
 }
